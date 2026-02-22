1059bff80ad2ee0db466aa1f62e0948bdd718302
Craig M <craigermct@gmail.com>
2026-02-22 13:58:45 -0500

    Add .gitignore, fix routes and auth

--- ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
+++ ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adefedc5067a3eef4e3214b5fad18cfa32a2f856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69d8847707445f046bc5210ff4b84e46d850c8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
