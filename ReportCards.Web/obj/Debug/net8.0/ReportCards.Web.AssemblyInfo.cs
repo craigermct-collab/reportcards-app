38f86a97a2dbbd62d26e94102bf423280d8f2a96
Craig M <craigermct@gmail.com>
2026-02-22 12:13:10 -0500

    Login Page Addition

--- ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
+++ ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a686a30de0c8e0d3651e6fcd43725f87d588add")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f4102cb467c620925b006f7f9c1e0d225d2a3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
