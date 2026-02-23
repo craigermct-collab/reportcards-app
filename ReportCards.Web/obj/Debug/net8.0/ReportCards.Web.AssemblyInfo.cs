edf7a359bdebeb4f6d5eebea39e3e86929b1f553
Craig M <craigermct@gmail.com>
2026-02-23 14:27:16 -0500

    Reorder homework checker steps: photos first, checks second, rubric context conditional

--- ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
+++ ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4b2a235171ca53c0989580ab630cedc89eb622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2f807ca58eda8304413a169b245df2566eea45")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
