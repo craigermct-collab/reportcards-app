99a18cb190bce05504fa93b51beb328be7349008
Craig M <craigermct@gmail.com>
2026-02-23 14:55:59 -0500

    Fix drawer toggle - add InteractiveServer rendermode to Routes and MainLayout

--- ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
+++ ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245a20aa9c9dfb006223a9e4803513781c8ec87a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7699558d9917a29268b950030b44c96cb0ea6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
