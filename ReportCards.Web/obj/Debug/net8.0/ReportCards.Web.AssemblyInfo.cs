e3beaf48b98aee1d312aaf6e4142136254a42495
Craig M <craigermct@gmail.com>
2026-02-23 08:58:11 -0500

    Pin user tile to bottom of drawer using flex layout

--- ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
+++ ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c725eb2831618e68ee43ac4d65d09ab57f788f09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03de00446f441a55d249687f780470e1cf4b5452")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
