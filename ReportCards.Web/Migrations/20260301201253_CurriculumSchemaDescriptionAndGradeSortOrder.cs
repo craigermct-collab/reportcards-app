44354ccaa70531e7ce47196ecb7750fb5289bd58
Craig M <craigermct@gmail.com>
2026-03-01 15:15:51 -0500

    Seed Ontario Curriculum 2026 template with grades JK-Grade5, 7 subjects, strands, and lorem sub-strands

---
+++ ReportCards.Web/Migrations/20260301201253_CurriculumSchemaDescriptionAndGradeSortOrder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportCards.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class CurriculumSchemaDescriptionAndGradeSortOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CurriculumSchemas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "CurriculumGradeTemplates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CurriculumSchemas");
+
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "CurriculumGradeTemplates");
+        }
+    }
+}
