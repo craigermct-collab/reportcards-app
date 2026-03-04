5bdf42afa6b89eb37f9342a1aaf8b097691c7df5
Craig M <craigermct@gmail.com>
2026-03-04 08:47:09 -0500

    Phase 3: Subject modifier template + strand activation UI on Class Setup

--- ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
+++ ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ff79309b8b70a66976deeb8b2655a743736c24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833f8684f66d139e121fd8f0be2fc666ee018a21")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
