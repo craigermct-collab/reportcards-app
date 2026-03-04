a247ab55d74e54e0094f0bef110ffca569fe3e96
Craig M <craigermct@gmail.com>
2026-03-04 08:55:17 -0500

    Phase 3: Collapse subject configuration section by default

--- ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
+++ ReportCards.Web/obj/Debug/net8.0/ReportCards.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833f8684f66d139e121fd8f0be2fc666ee018a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3cb816cf0c2ba96ede1eb21c98883368fc8833f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportCards.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
